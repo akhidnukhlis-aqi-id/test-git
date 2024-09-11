6d2aeae889f292dc464afc2a020e688a7c6b389d
Aulia Annisa R <rahmatillah.aa@outlook.com>
2024-09-11 21:27:48 +0800

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,3 @@
 <?php
-    echo "Mungkin Takut Perubahan";
-    echo "Semua Orang Pernah Sakit Hati";
-    echo "Ribuan Memori";
-    echo "Hati & Paru Paru";
-    echo "Echo 5x";
+    echo "markicob"
 ?>
