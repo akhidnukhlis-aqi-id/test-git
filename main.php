68d1189f4dd30cfc73cb1249785125d611e57502
18221171 <18221171@std.stei.itb.ac.id>
2024-09-11 20:03:42 +0700

    fix: change echo content

--- main.php
+++ main.php
@@ -1,4 +1,7 @@
 <?php
-    echo "Hello Jhon Doe!!";
-    echo "ini message";
+    echo "Mungkin Takut Perubahan";
+    echo "Semua Orang Pernah Sakit Hati";
+    echo "Ribuan Memori";
+    echo "Hati & Paru Paru";
+    echo "Echo 5x";
 ?>
